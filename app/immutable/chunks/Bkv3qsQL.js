fb5ee6ad0c410c73a71d446efbfc5d5181322812
fadeenk <fadeekannah@gmail.com>
2025-05-15 03:54:31 -0700

    Updates

---
+++ app/immutable/chunks/Bkv3qsQL.js
@@ -0,0 +1,1 @@
+var e;const a=((e=globalThis.__sveltekit_1wjkra7)==null?void 0:e.base)??"/meetinghero.ai";var s;(s=globalThis.__sveltekit_1wjkra7)==null||s.assets;export{a as b};
--- app/immutable/chunks/C_JkGfJB.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./CbdMBRV-.js";e();
